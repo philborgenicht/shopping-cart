552d81379c5873d42df25630302c306676a72d39
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-07-05 12:38:22 -0600

    passed props into CartFooter and rendered the appropriate data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
       <div className="App">
         <CartHeader/>
         <CartItems/>
-        <CartFooter/>
+        <CartFooter
+          copyright="2016"
+        />
       </div>
     );
   }
