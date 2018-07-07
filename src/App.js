4f7adaf2726091d78018cf87a0ae2a343cd57748
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-07-07 15:05:38 -0600

    completed shopping cart using data stored in state with ability to add items to state and populate the cart

--- src/App.js
+++ src/App.js
@@ -2,20 +2,56 @@ import React, { Component } from 'react'
 import CartHeader from './CartHeader'
 import CartFooter from './CartFooter'
 import CartItems from './CartItems'
-import CartItem from './CartItem'
+import AddItem from './AddItem'
 
+const cartItemsList = [
+  { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+  { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+  { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+]
+
+const products = [
+  { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+  { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+  { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+  { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+  { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+  { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+  { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+  { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+  { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+]
 class App extends Component {
+
+
+  addToList = (elem) => {
+    const newItem = products.filter(item => item.name === elem.product)[0]
+    const id = this.state.cartItemsList[this.state.cartItemsList.length-1].id+1
+    const finalItem = {
+      id: id,
+      product: newItem,
+      quantity: elem.quantity
+    }
+    this.setState({cartItemsList: [...this.state.cartItemsList, finalItem]})
+    // console.log('this.state: ',this.state)
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {cartItemsList: cartItemsList}
+  }
+
+
   render() {
-    const cartItemsList = [
-      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
-    ]
     return (
       <div className="App">
         <CartHeader/>
         <CartItems
-          list={cartItemsList}
+          list={this.state.cartItemsList}
+        />
+        <AddItem
+          products={products}
+          addToList={this.addToList}
         />
         <CartFooter
           copyright="2016"
